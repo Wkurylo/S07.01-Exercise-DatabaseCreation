6d05c7e18419635ba8d2eee064ed5d8c70971ade
wkurylo88@gmail.com <wkurylo88@gmail.com>
2017-12-14 10:19:05 +0100

    * completed

--- app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -15,19 +15,41 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Manages a local database for weather data.
  */
-// TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-public class WeatherDbHelper {
+// completed (11) Extend SQLiteOpenHelper from WeatherDbHelper
+public class WeatherDbHelper extends SQLiteOpenHelper{
 
-//  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "weather.db";
 
-//  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
 
-//  TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
+    public WeatherDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-//  TODO (15) Override onCreate and create the weather table from within it
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-//  TODO (16) Override onUpgrade, but don't do anything within it yet
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+//  completed (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+
+//  completed (13) Create a private static final int called DATABASE_VERSION and set it to 1
+
+//  completed (14) Create a constructor that accepts a context and call through to the superclass constructor
+
+//  completed (15) Override onCreate and create the weather table from within it
+
+//  completed (16) Override onUpgrade, but don't do anything within it yet
 }
